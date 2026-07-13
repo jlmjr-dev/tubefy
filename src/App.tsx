fb4c5dc4bc247def417ac1d0492f04452088b94d
jlmjr-dev <joseluizmonteirojr2@gmail.com>
2026-07-13 11:18:39 -0400

    mocking playlist items in place, folder grouping also

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import { Login } from "@/pages/login"
 // import { Landing } from "@/pages/landing"
+import { Playlists } from "@/pages/playlists/list"
 
 export function App() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       {/* <Login /> */}
       {/* <Landing /> */}
+      <Playlists />
     </div>
   )
 }
